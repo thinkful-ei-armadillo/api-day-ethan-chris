06e828dcec5b901bedb288d6e38f34ac0a72d602
Ethan Zimmerman <thebinarypenguin@gmail.com>
2019-01-28 12:28:54 -0500

    Fetch the items and add them to the store

--- scripts/index.js
+++ scripts/index.js
@@ -5,10 +5,14 @@
 $(document).ready(function() {
   shoppingList.bindEventListeners();
   shoppingList.render();
+
+  api.getItems()
+    .then(res => res.json())
+    .then((items) => {
+      items.forEach((item) => store.addItem(item));
+      shoppingList.render();
+    });
+
 });
 
-store.items.push(Item.create('apples'));
-
-api.getItems(); 
-
-
+api.getItems();
